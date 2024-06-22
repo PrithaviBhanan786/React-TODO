6a79855161cae65c9242b231e20f38a409cd36f8
Prithavi Bhanan <166564154+PrithaviBhanan786@users.noreply.github.com>
2024-06-22 17:16:01 +0530

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import "./Todo.css";
+
+const LOCAL_STORAGE_KEY = "react-todo-list-task";
+
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  // For Adding TODO
+  const addTodo = (todo) => {
+    if (!todo.text) return;
+    setTodos([todo, ...todos]);
+  };
+
+  // For Removing TODO
+  const removeTodo = (id) => {
+    const removeArray = todos.filter((todo) => todo.id !== id);
+    setTodos(removeArray);
+  };
+
+  // For Updating TODO
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text) return;
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  // For Completed Tasks
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <section className="main-container">
+      <h1 className="todolist-header">ToDo List</h1>
+      <TodoForm onSubmit={addTodo} />
+      <TodoList
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </section>
+  );
+};
+
+export default Todo;
